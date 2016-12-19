b1deeb4093c407663dab7bcfdf2d22a0fc7ee3d7
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-12-19 08:11:58 -0500

    minor error db still not working

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Sing Up</title>
+<title>Sign Up</title>
 <style>
 label{
 width:100px;
